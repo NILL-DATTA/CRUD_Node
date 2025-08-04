4e309af95e6265d18096a6a0bafc47ad536da40c
NILL-DATTA <88128953+NILL-DATTA@users.noreply.github.com>
2025-08-04 18:25:14 +0530

    updatre:

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
